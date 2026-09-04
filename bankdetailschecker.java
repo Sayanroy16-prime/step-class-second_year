2a7b574ecf48c6cb241786d8486c34d8f4de2bbb
Sayanroy <Sayanroy090666>
2026-09-04 22:37:39 +0530

    Copy all week2class files into class folder of week2 branch

---
+++ bankdetailschecker.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class bankdetailschecker {
+
+    public static void main(String[] args){
+        String bankDetails;
+        Scanner sc = new Scanner(System.in);
+
+
+         
+    }
+    
+}
